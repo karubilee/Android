eab6377844360881e16b39773f12caf00c271322
lb <karubi@karubi-2.local>
2017-09-08 17:18:06 +0800

    ᕕ(ᐛ)ᕗ ActivityTest

--- HelloWorld_Android/app/src/main/java/com/example/karubi/helloworld_android/HelloWorldActivity.java
+++ HelloWorld_Android/app/src/main/java/com/example/karubi/helloworld_android/HelloWorldActivity.java
@@ -2,12 +2,16 @@ package com.example.karubi.helloworld_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
-
+    private static final String TAG = "HelloWorldActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        Log.d("HelloWorldActivity","onCreate execute");
+        Log.i(TAG, "onCreate: ");
+
     }
 }
