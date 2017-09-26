3effc5861718c463d99e10cbcc228bb02ae85175
lb <karubi@karubi-2.local>
2017-09-26 10:45:52 +0800

    第3章搞掂.开始第4章

---
+++ RecyclerView/app/src/main/java/com/example/karubi/recyclerview/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.karubi.recyclerview;
+
+/**
+ * Created by karubi on 2017/9/20.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit (String name,int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
