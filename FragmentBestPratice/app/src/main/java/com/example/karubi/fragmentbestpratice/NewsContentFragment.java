2b6301d13a11a8ec02e33cf196a02b910824d6b9
lb <karubi@karubi-2.local>
2017-09-29 15:56:29 +0800

    ᕕ(ᐛ)ᕗ简易新闻界面.

---
+++ FragmentBestPratice/app/src/main/java/com/example/karubi/fragmentbestpratice/NewsContentFragment.java
@@ -0,0 +1,33 @@
+package com.example.karubi.fragmentbestpratice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by karubi on 2017/9/29.
+ */
+
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag, container, false);
+        return view;
+    }
+
+    public void refresh(String newsTitle,String newsContent) {
+        View visbilityLayout = view.findViewById(R.id.visibility_layout);
+        visbilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView) view.findViewById(R.id.news_title);
+        TextView newsContentText = (TextView) view.findViewById(R.id.news_content);
+        newsTitleText.setText(newsTitle);
+        newsContentText.setText(newsContent);
+    }
+}
