c8bafe65b3bf06ded05e1209fc3e57cb6fe8a323
lb <karubi@karubi-2.local>
2017-09-30 17:13:44 +0800

    ᕕ(ᐛ)ᕗBroadcastPractice 强制下线功能

---
+++ BroadcastBestPractice/app/src/main/java/com/example/karubi/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.karubi.broadcastbestpractice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+                Log.d("MainActivity", "onClick: ");
+            }
+        });
+    }
+}
---
+++ BroadcastTest/app/src/main/java/com/example/karubi/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,20 @@
+package com.example.karubi.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    public BootCompleteReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"Boot complete",Toast.LENGTH_SHORT).show();
+        Log.d("BootCompleteReceiver", "onReceive: ");
+    }
+}
